7bdf3376387681c22b6437433ac8592907ec9db2
Ed Rivas <ed@unplug.studio>
2020-06-05 11:32:50 -0600

    fix: let WordPress manage favicon. Closes #11

--- header.php
+++ header.php
@@ -7,10 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <?php if(get_field('favicon', 'option')): ?>
-      <link rel="icon" href="<?= get_field('favicon', 'option') ?>">
-    <?php endif ?>
-
     <?php wp_head(); ?>
   </head>
 
