e01ac34897d2da112f7927a9f125201c32f79332
Anibal <androso421@gmail.com>
2022-06-09 13:40:19 -0600

    switching from node-sass (lib-sass) to dart-sass

--- web/app/themes/custom/assets/webpack.config.js
+++ web/app/themes/custom/assets/webpack.config.js
@@ -1,37 +1,37 @@
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const globImporter = require('node-sass-glob-importer')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const { resolve } = require('path')
-const WebpackAssetsManifestPlugin = require('webpack-assets-manifest')
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const globImporter = require("node-sass-glob-importer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { resolve } = require("path");
+const WebpackAssetsManifestPlugin = require("webpack-assets-manifest");
 
 module.exports = (env, argv) => {
-  const isDev = argv.mode === 'development'
-  const options = { sourceMap: isDev }
+  const isDev = argv.mode === "development";
+  const options = { sourceMap: isDev };
 
   const devPlugins = [
     new BrowserSyncPlugin({
       // Set your desired proxy when calling webpack
       // npm start -- --env.proxy example.com
       proxy: env && env.proxy,
-      open: false
-    })
-  ]
+      open: false,
+    }),
+  ];
   const prodPlugins = [
     new WebpackAssetsManifestPlugin(),
-    new OptimizeCssAssetsPlugin()
-  ]
+    new OptimizeCssAssetsPlugin(),
+  ];
   const plugins = [
     new MiniCssExtractPlugin({
-      filename: isDev ? '[name].css' : '[name].[chunkhash].css'
-    })
-  ].concat(isDev ? devPlugins : prodPlugins)
+      filename: isDev ? "[name].css" : "[name].[chunkhash].css",
+    }),
+  ].concat(isDev ? devPlugins : prodPlugins);
 
   return {
-    entry: { theme: './js/index.js' },
+    entry: { theme: "./js/index.js" },
     output: {
-      path: resolve(__dirname, 'dist'),
-      filename: isDev ? '[name].js' : '[name].[chunkhash].js'
+      path: resolve(__dirname, "dist"),
+      filename: isDev ? "[name].js" : "[name].[chunkhash].js",
     },
     module: {
       rules: [
@@ -39,29 +39,31 @@ module.exports = (env, argv) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader'
-          }
+            loader: "babel-loader",
+          },
         },
         {
           test: /\.s?css$/,
           use: [
             { loader: MiniCssExtractPlugin.loader, options },
-            { loader: 'css-loader', options },
-            { loader: 'postcss-loader', options },
+            { loader: "css-loader", options },
+            { loader: "postcss-loader", options },
             {
-              loader: 'sass-loader',
+              loader: "sass-loader",
               options: {
                 ...options,
-                sassOptions: { importer: globImporter() }
-              }
-            }
-          ]
+                // Prefer dart sass
+                implementation: require("sass"),
+                sassOptions: { importer: globImporter() },
+              },
+            },
+          ],
         },
         {
           test: /\.(jpe?g|png|gif|eot|svg|ttf|woff|woff2|mp4|webm)$/,
-          loader: 'file-loader'
-        }
-      ]
+          loader: "file-loader",
+        },
+      ],
     },
     plugins,
     stats: {
@@ -70,8 +72,8 @@ module.exports = (env, argv) => {
       colors: true,
       hash: false,
       modules: false,
-      version: false
+      version: false,
     },
-    devtool: isDev ? 'source-map' : false
-  }
-}
+    devtool: isDev ? "source-map" : false,
+  };
+};
